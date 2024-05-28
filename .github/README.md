0623fd2a9fdae654742f1dedd475e41e4d4e3457
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-05-27 22:02:38 -0400

    init

--- .github/README.md
+++ .github/README.md
@@ -1,20 +1,14 @@
-# Nick's Preferences (MacOS)
+# Nick's Preferences (Debian Linux)
 
-preferences is a script - written in pure bash - to set up a macOS computer with my opinionated configurations.
+preferences is a script - written in pure bash - to set up a *Debian* Linux computer with my opinionated configurations.
 
 It can run multiple times on the same machine safely.
-
-[Requirements](https://github.com/dotbrains/preferences#requirements) / [Install](https://github.com/dotbrains/preferences#install) / [License](https://github.com/dotbrains/preferences#license)
+[Requirements](https://github.com/dotbrains/preferences/blob/debian/README.md#requirements) / [Install](https://github.com/dotbrains/preferences/blob/debian/README.md#install) / [License](https://github.com/dotbrains/preferences/blob/debian/README.md#license)
 
 Requirements
 ------------
 
-macOS supports:
-
-* macOS Sonoma (14.5)
-
-Older versions may work but aren't regularly tested. Bug reports for older
-versions are welcome.
+A computer that is running *Debian* Linux.
 
 Install
 -------
@@ -22,7 +16,7 @@ Install
 Download, review, then execute the script:
 
 ```
-source /dev/stdin <<<"$(curl -s "https://raw.githubusercontent.com/dotbrains/preferences/master/preferences.sh")"
+source <(curl -s "https://raw.githubusercontent.com/dotbrains/blob/debian/preferences.sh")
 ```
 
 It should take less than a minute to install.
--- colorschemes/nord/nord.sh
+++ colorschemes/nord/nord.sh
@@ -11,31 +11,36 @@ declare current_dir &&
 
 main() {
 
-    brew_bundle_install -f "brewfile"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     fish -c "printf \"y\n\" | fish_config theme save \"Nord\""
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # Nord for terminal on MacOS
-
-    ./apps/terminal/set_terminal_theme.applescript
+    # The following steps are added for GNOME-based OSs only
+    # We don't want this to execute if we're not on a GNOME-based OSs.
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    # Nord for iTerm2 on MacOS
+    # Nord for gnome-terminal
+    # see: https://github.com/nordtheme/gnome-terminal
 
-    ./apps/iterm2/set_iterm2_theme.applescript
+    # Check if the gnome-terminal command exists
+    if cmd_exists "gnome-terminal"; then
+        git clone https://github.com/nordtheme/gnome-terminal.git
+
+        cd gnome-terminal/src || exit 1
+
+        ./nord.sh -p Nord
+    fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Set the desktop background to the Nord wallpaper
 
-    # Check if the `wallpaper` command exists
-    if cmd_exists "wallpaper"; then
-        wallpaper set "${current_dir}/wallpapers/magma.jpg"
+    # Check if the `gsettings` command exists
+    if cmd_exists "gsettings"; then
+        gsettings \
+            set org.gnome.desktop.background \
+            picture-uri "file://$HOME/set-me-up/dotfiles/modules/nord/wallpaper/magma.jpg"
     fi
 
 }
