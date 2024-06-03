740578a255d7ff3a3f00f804b62546cd1b4a916a
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-06-02 20:16:14 -0400

    fixed issues with system/security.sh

--- system/security.sh
+++ system/security.sh
@@ -9,7 +9,14 @@ ask_for_sudo
 # Use Touch ID to authenticate sudo commands
 # https://apple.stackexchange.com/a/466029
 
+# Check if /etc/pam.d/sudo_local exists, create it from the template if it doesn't
+if [ ! -f /etc/pam.d/sudo_local ]; then
+    sudo cp /etc/pam.d/sudo_local.template /etc/pam.d/sudo_local
+fi
+
 # Check if the line is already uncommented in /etc/pam.d/sudo_local
 if ! grep -q "^auth" /etc/pam.d/sudo_local; then
-    sed "s/^#auth/auth/" /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local >/dev/null
+    sudo sed -i '' "s/^#auth/auth/" /etc/pam.d/sudo_local
 fi
+
+echo "Updated /etc/pam.d/sudo_local to use Touch ID for sudo commands."
